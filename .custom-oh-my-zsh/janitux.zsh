dff94bd3aca7fd61c6be818d6a6a16dca74a2694
Patricio Pérez <pperez@boaboa.org>
2016-02-15 18:20:03 -0300

    ADD: Commit inicial dotfiles
    Signed-off-by: Patricio Pérez <pperez@boaboa.org>

---
+++ .custom-oh-my-zsh/janitux.zsh
@@ -0,0 +1,3 @@
+unsetopt share_history # Personalmente me marea ver el historial de las otras sesiones
+
+zstyle ":completion:*:commands" rehash 1 # Tener disponibles los comandos a penas esten disponibles
