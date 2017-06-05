8fe0d16ea6ecf930c0b3e5c0ada59a285ff9a099
Patricio Pérez <pperez@boaboa.org>
2017-06-04 22:29:50 -0400

    DEL: Quitar java home
    Signed-off-by: Patricio Pérez <pperez@boaboa.org>

--- .custom-oh-my-zsh/janitux.zsh
+++
@@ -1,9 +0,0 @@
-#
-## Settings personales ZSH
-#
-
-# Deshabilitar historial de otras sesiones de zsh
-unsetopt share_history
-
-# Los comandos estan disponibles en seguida, sin tener que recargar la shell
-zstyle ":completion:*:commands" rehash 1
