3fca0dff383f28c710c73a1a04e9ec0af5611ae9
Patricio Pérez <pperez@boaboa.org>
2017-06-12 19:25:42 -0400

    ADD: Agregar mis configs personales perdidas
    Signed-off-by: Patricio Pérez <pperez@boaboa.org>

---
+++ .custom-oh-my-zsh/pato.zsh
@@ -0,0 +1,9 @@
+#
+## Settings personales ZSH
+#
+
+# Deshabilitar historial de otras sesiones de zsh
+unsetopt share_history
+
+# Los comandos estan disponibles en seguida, sin tener que recargar la shell
+zstyle ":completion:*:commands" rehash 1
